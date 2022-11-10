407d7c016528635b7e9c618fe5f859eaf9a74b81
anjanayasodharan89@gmail.com <anjanayasodharan89@gmail.com>
2022-11-10 22:02:08 +0530

    change commit

--- src/Admin.java
+++ src/Admin.java
@@ -1,16 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Scanner;
+import java.sql.*;
 
 public class Admin {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/admindb","root","");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
         int choice;
         while (true) {
-            System.out.println("select an option");
-            System.out.println("1.insert");
-            System.out.println("2.select");
-            System.out.println("3.search");
-            System.out.println("4.delete");
-            System.out.println("5.update");
-            System.out.println("6.Exit");
+            System.out.println("select an option from the menu");
+            System.out.println("1 add consumer")
+            System.out.println("2 search consumer ")
+            System.out.println("3 delete consumer")
+            System.out.println("4 update consumer")
+            System.out.println("5 view all consumer")
+            System.out.println("6 generate bill")
+            System.out.println("7 view bill")
+            System.out.println("8 Top 2 high bill")
+            System.out.println("9 exit")
             Scanner scanner = new Scanner(System.in);
             choice = scanner.nextInt();
             switch (choice) {
