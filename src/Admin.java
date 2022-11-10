6ddb649ddaecc17d5f249d81b60d5e4a00eaf0de
anjanayasodharan89@gmail.com <anjanayasodharan89@gmail.com>
2022-11-10 22:06:44 +0530

    second commit

--- src/Admin.java
+++ src/Admin.java
@@ -15,34 +15,43 @@ public class Admin {
         int choice;
         while (true) {
             System.out.println("select an option from the menu");
-            System.out.println("1 add consumer")
-            System.out.println("2 search consumer ")
-            System.out.println("3 delete consumer")
-            System.out.println("4 update consumer")
-            System.out.println("5 view all consumer")
-            System.out.println("6 generate bill")
-            System.out.println("7 view bill")
-            System.out.println("8 Top 2 high bill")
-            System.out.println("9 exit")
+            System.out.println("1 add consumer");
+            System.out.println("2 search consumer ");
+            System.out.println("3 delete consumer");
+            System.out.println("4 update consumer");
+            System.out.println("5 view all consumer");
+            System.out.println("6 generate bill");
+            System.out.println("7 view bill");
+            System.out.println("8 Top 2 high bill");
+            System.out.println("9 exit");
             Scanner scanner = new Scanner(System.in);
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("insert student selected");
+                    System.out.println("insert consumer selected");
                     break;
                 case 2:
-                    System.out.println("select student selected");
+                    System.out.println("search consumer selected");
                     break;
                 case 3:
-                    System.out.println("search student selected");
+                    System.out.println("delete consumer selected");
                     break;
                 case 4:
-                    System.out.println("delete student selected");
+                    System.out.println("update consumer selected");
                     break;
                 case 5:
-                    System.out.println("update student selected");
+                    System.out.println("view all consumer selected");
                     break;
                 case 6:
+                    System.out.println("generate bill");
+                    break;
+                case 7:
+                    System.out.println("view bill");
+                    break;
+                case 8:
+                    System.out.println("Top 2 high bill");
+                    break;
+                case 9:
                     System.exit(0);
                     break;
             }
